762931cb60f0e070753c7d7c4980ee772454c661
John Doe <shepherd.munemo@outlook.com>
2022-11-10 12:14:18 +0200

    Update

---
+++ src/CoMute/Data/CoMuteDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CoMute.Web.Data
+{
+    public class CoMuteDbContext : DbContext
+    {
+            public DbSet<User> Users { get; set; }
+            public DbSet<CarPool> CarPools { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["DevConnection"].ConnectionString);
+            }
+    }
+}
---
+++ src/CoMute/Models/CarPool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoMute.Web.Models
+{
+    public class CarPool
+    {
+        [Required]
+        public string Destination { get; set; }
+        [Required]
+        public string Origin { get; set; }
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime DepatureTime { get; set; }
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime ExpectedArrival { get; set; }
+        public string DaysAvailable { get; set; }
+        [Required]
+        public int AvailableSeats { get; set; }
+        [Required]
+        public string Leader { get; set; }
+        public string Notes { get; set; }
+    }
+}
